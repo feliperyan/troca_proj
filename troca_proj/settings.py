f9bb3a0fab3187503da5622ecef0cdd0aabb9cf3
Felipe Ryan <feliperyan@gmail.com>
2013-05-04 01:03:27 +1000

    media and static settings were getting overwritten

--- troca_proj/settings.py
+++ troca_proj/settings.py
@@ -7,6 +7,26 @@ SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 MEDIA_PATH = os.path.join(SITE_ROOT, '../media')
 TEMPLATES_PATH = os.path.join(SITE_ROOT, 'templates')
 
+MEDIA_ROOT = MEDIA_PATH 
+MEDIA_URL = '/media/'
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
 FACEBOOK_API_KEY = '112921768860216'
 FACEBOOK_APP_ID = '112921768860216'
 FACEBOOK_APP_SECRET = '32c7334cbf6811abdf6d8579e59c0cac'
@@ -100,39 +120,6 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = MEDIA_PATH 
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'njbw-3ksmuhus4s9o=ezevc+mfle9vr&amp;e23x1yp9o-*-tizr$^'
